6e70d1b5a41a752b9866973ea75e31e0921f8e27
Rahul Saini <142562929+rahul-saini-code@users.noreply.github.com>
2026-05-01 11:54:53 +0530

    Create max_element.cpp

---
+++ Array/max_element.cpp
@@ -0,0 +1,22 @@
+/*Given an array of integers, find the maximum element in the array.
+arr = [3, 7, 2, 9, 4]*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+int arr[] = {3 , 7 , 2 , 9 , 4};
+
+//cout << arr[0] <<endl;
+
+int max = arr[0];
+int length = sizeof(arr);
+
+for(int i = 1 ; i < length ; i++){
+    if(max < arr[i]){
+        max = arr[i];
+    }
+}
+cout << "Max element in this array is " << max << endl;
+return 0;
+}
