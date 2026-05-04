e315494191547253bf4d1d19950036f56b021783
Rahul Saini <142562929+rahul-saini-code@users.noreply.github.com>
2026-05-04 10:31:46 +0530

    Create pract_4.cpp

---
+++ Array/searching/pract_4.cpp
@@ -0,0 +1,22 @@
+//Find first occurrence index of target
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {4, 2, 7, 2, 9};
+    int target = 2 ;
+    int length = sizeof(arr) / sizeof(arr[0]);
+    int count = 0;
+    
+    for(int i = 0 ; i < length ; i++){
+        if(arr[i] == target){
+            cout << i;
+            count++;
+            break;
+        }
+    }
+    if(count == 0){
+        cout << "Element not found";
+    } 
+    return 0;
+}
